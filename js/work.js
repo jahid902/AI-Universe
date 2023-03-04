339e4248f74bc3e02967734b7ec7430ef024e2d5
jahid902 <jahidjr902@gmail.com>
2023-03-04 22:28:19 +0600

    fixed some issue

--- js/work.js
+++ js/work.js
@@ -60,7 +60,8 @@ const showAll = () => {
 const singleCardCatch = (id) =>{
     fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`)
     .then(res => res.json())
-    .then(data => singleCardDetail(data.data))
+    .then(data => {singleCardDetail(data.data)
+    console.log(data.data)})
     
 }
 // modal function 
@@ -73,10 +74,11 @@ const containerDiv = document.getElementById("modal-body");
   
       <div class="col-12 col-md-4 position-relative mt-3">
       <img src="${data.image_link[0]}" class="img-fluid rounded-start" alt="...">
-      <button id="percentage-btn" class="p-1 mt-2 btn btn-danger bg-opacity-25 position-absolute top-0 end-0">${data.accuracy.score  ? Math.round(data.accuracy.score*100) : "" } % accuracy </button>
+      <button id="percentage-btn" class="p-1 mt-2 btn btn-danger bg-opacity-25 position-absolute top-0 end-0">
+      ${data.accuracy?.score  ? Math.round(data.accuracy?.score*100) : ""} % accuracy </button>
       <br>
-      <h4 class="mt-2 px-2">${data.input_output_examples[0].input ? data.input_output_examples[0].input : data.input_output_examples[1].input}</h4>
-      <p class="mt-2 px-2">${data.input_output_examples[0].output ? data.input_output_examples[0].output : data.input_output_examples[1].output }</p>
+      <h4 class="mt-2 px-2">${data.input_output_examples?.[0].input ? data?.input_output_examples?.[0].input : "no data available"}</h4>
+      <p class="mt-2 px-2">${data.input_output_examples?.[0].output ? data?.input_output_examples?.[0].output : "no data available" }</p>
       </div>
 
       <div class="col-12 col-md-8">
@@ -84,25 +86,25 @@ const containerDiv = document.getElementById("modal-body");
       <div class="bg-danger bg-opacity-10 p-3">
       <h4 class="card-title">${data.description}</h4>
       <div class="d-flex justify-content-center justify-content-md-between">
-      <button type="button" class="btn btn-light text-success">${data.pricing[0].price ? data.pricing[0].price : "free to use"} <br> ${data.pricing[0].plan ? data.pricing[0].plan : "Always free"}</button>
-      <button type="button" class="btn btn-light text-warning-emphasis">${data.pricing[1].price ? data.pricing[1].price : "free to use"} <br> ${data.pricing[1].plan ? data.pricing[1].plan : "Always free"}</button>
-      <button type="button" class="btn btn-light text-danger-emphasis">${data.pricing[2].price ? data.pricing[2].price : "free to use"} <br> ${data.pricing[2].plan ? data.pricing[2].plan : "Always free"}</button>    
+      <button type="button" class="btn btn-light text-success">${data.pricing?.[0].price ? data.pricing?.[0].price : "free to use"} <br> ${data.pricing?.[0].plan ? data.pricing?.[0].plan : "Always free"}</button>
+      <button type="button" class="btn btn-light text-warning-emphasis">${data.pricing?.[1].price ? data.pricing?.[1].price : "free to use"} <br> ${data.pricing?.[1].plan ? data.pricing?.[1].plan : "Always free"}</button>
+      <button type="button" class="btn btn-light text-danger-emphasis">${data.pricing?.[2].price ? data.pricing?.[2].price : "free to use"} <br> ${data.pricing?.[2].plan ? data.pricing?.[2].plan : "Always free"}</button>    
       </div>
       <div class="d-flex justify-content-between mt-4">
       <div>
       <h5>Features</h5>
       <ul>
-          <li>${data.features["1"].feature_name ? data.features["1"].feature_name : "no data available"}</li>
-          <li>${data.features["2"].feature_name ? data.features["2"].feature_name : "no data available"}</li>
-          <li>${data.features["3"].feature_name ? data.features["3"].feature_name : "no data available"}</li>
+          <li>${data.features[1].feature_name ? data.features[1].feature_name : "no data available"}</li>
+          <li>${data.features[2].feature_name ? data.features[2].feature_name : "no data available"}</li>
+          <li>${data.features[3].feature_name ? data.features[3].feature_name : "no data available"}</li>
       </ul>
       </div>
       <div>
       <h5>Integrations</h5>
       <ul>
-          <li>${data.integrations[0] ? data.integrations[0] : "no data available"}</li>
-          <li>${data.integrations[1] ? data.integrations[1] : "no data available"}</li>
-          <li>${data.integrations[2] ? data.integrations[2] : "no data available"}</li>
+          <li>${data.integrations?.[0] ? data.integrations?.[0] : "no data available"}</li>
+          <li>${data.integrations?.[1] ? data.integrations?.[1] : "no data available"}</li>
+          <li>${data.integrations?.[2] ? data.integrations?.[2] : "no data available"}</li>
       </ul>
       </div>
       </div>
@@ -124,30 +126,3 @@ const loader = document.getElementById("spinner");
   }
 
 }
-
-document.getElementById("sort-by").addEventListener('click', function(){
-
-  const URL = `https://openapi.programming-hero.com/api/ai/tools`;
-    fetch(URL)
-    .then(res => res.json())
-    .then(data => {sorted(data.data.tools)
-    // console.log(data.data.tools)
-    })
-})
-
-const sorted = (datas) =>{
-  
-  datas.map(data => {
-    const dates = data.published_in;
-    console.log(data)
-    // const filteredDate = dates.filter(date => date >= 6/1/2009);         
-    // console.log(filteredDate);   
-
-  })
-
-  
-}
-
-
-
-
