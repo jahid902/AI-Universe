50c6ab7c1ac22825197c35c3d8797168ec1a39ab
jahid902 <jahidjr902@gmail.com>
2023-03-04 12:46:13 +0600

    completed modal

--- js/work.js
+++ js/work.js
@@ -19,7 +19,7 @@ const showCardData = (data) => {
                       <ol>
                       <li>${cardInfo.features[0]}</li>
                       <li>${cardInfo.features[1]}</li>
-                      <li>${cardInfo.features[2]}</li>                     
+                      <li>${cardInfo.features[2] ? cardInfo.features[2] : "not available"}</li>                     
                       </ol>
                     </div>
                     <hr>
@@ -29,8 +29,10 @@ const showCardData = (data) => {
                          <i class="fa-sharp fa-solid fa-calendar-days"></i>                                   
                     <p>${cardInfo.published_in}</p> 
                     </div>
-                    <div>
-                        <i class="fa-sharp fa-solid fa-arrow-right fs-3"></i>
+                    <div>                       
+                        <button onclick="singleCardCatch('${cardInfo.id}')" class="border border-0 text-danger bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <i class="fa-sharp fa-solid fa-arrow-right fs-4"></i>
+                        </button>
                     </div>                                                      
                     </div>
 
@@ -53,8 +55,63 @@ const showAll = () => {
 
 }
 
+const singleCardCatch = (id) =>{
+    fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`)
+    .then(res => res.json())
+    .then(data => {singleCardDetail(data.data)
+    console.log(data.data);
+    })
+    
+}
 
+const singleCardDetail = (data) =>{
 
+   const containerDiv = document.getElementById("modal-body");
+   containerDiv.innerHTML =`
+   <div class="card mb-3" style="max-width: 100%;">
+  <div class="d-flex flex-row-reverse g-0">
+
+    <div class="col-md-4 position-relative mt-3">
+      <img src="${data.image_link[0]}" class="img-fluid rounded-start" alt="...">
+      <button class="p-1 btn btn-danger bg-opacity-25 position-absolute top-0 end-0">${data.accuracy.score ? data.accuracy.score : " "} % accuracy </button>
+      <br>
+      <h4 class="mt-2 px-2">${data.input_output_examples[0].input ? data.input_output_examples[0].input : data.input_output_examples[1].input}</h4>
+      <p class="mt-2 px-2">${data.input_output_examples[0].output ? data.input_output_examples[0].output : data.input_output_examples[1].output }</p>
+    </div>
+
+    <div class="col-md-8">
+      <div class="card-body">
+      <div class="bg-danger bg-opacity-10 p-3">
+      <h4 class="card-title">${data.description}</h4>
+      <div class="d-flex justify-content-between">
+      <button type="button" class="btn btn-light text-success">${data.pricing[0].price ? data.pricing[0].price : "free to use"} <br> ${data.pricing[0].plan ? data.pricing[0].plan : "Always free"}</button>
+      <button type="button" class="btn btn-light text-warning-emphasis">${data.pricing[1].price ? data.pricing[1].price : "free to use"} <br> ${data.pricing[1].plan ? data.pricing[1].plan : "Always free"}</button>
+      <button type="button" class="btn btn-light text-danger-emphasis">${data.pricing[2].price ? data.pricing[2].price : "free to use"} <br> ${data.pricing[2].plan ? data.pricing[2].plan : "Always free"}</button>    
+      </div>
+      <div class="d-flex justify-content-between mt-4">
+      <div>
+      <h5>Features</h5>
+      <ul>
+              <li>${data.features["1"].feature_name ? data.features["1"].feature_name : "no data available"}</li>
+              <li>${data.features["2"].feature_name ? data.features["2"].feature_name : "no data available"}</li>
+              <li>${data.features["3"].feature_name ? data.features["3"].feature_name : "no data available"}</li>
+      </ul>
+      </div>
+      <div>
+      <h5>Integrations</h5>
+      <ul>
+              <li>${data.integrations[0] ? data.integrations[0] : "no data available"}</li>
+              <li>${data.integrations[1] ? data.integrations[1] : "no data available"}</li>
+              <li>${data.integrations[2] ? data.integrations[2] : "no data available"}</li>
+      </ul>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+     `
+}
 
 
 
